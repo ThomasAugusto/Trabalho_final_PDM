500a8c8b167a3159fa2f2d073544c587b4827312
ThomasAugusto <thomas.ramos@estudante.senairs.edu.br>
2023-11-07 22:15:19 -0300

    version 1.0.0

---
+++ app/src/main/java/com/example/trabalho_final_pdm/Login_form.java
@@ -0,0 +1,14 @@
+package com.example.trabalho_final_pdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+}
