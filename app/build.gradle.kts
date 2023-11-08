c8f9a6f21ccab4f816a42b60339edc9cee22c6f7
ThomasAugusto <thomas.ramos@estudante.senairs.edu.br>
2023-11-08 20:44:07 -0300

    version 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.trabalho_final_pdm"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.trabalho_final_pdm"
--- app/src/main/java/com/example/trabalho_final_pdm/Login_form.java
+++ app/src/main/java/com/example/trabalho_final_pdm/Login_form.java
@@ -2,13 +2,30 @@ package com.example.trabalho_final_pdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login_form extends AppCompatActivity {
 
+    private TextView text_sing_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        StartComponents();
+
+        text_sing_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_form.this, SingUp_form.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void StartComponents(){
+        text_sing_up = findViewById(R.id.TextView_login_singup);
     }
 }
