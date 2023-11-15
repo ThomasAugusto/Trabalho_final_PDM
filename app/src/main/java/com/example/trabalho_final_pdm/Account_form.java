216f1d2b8a28e020a6dcadab9035237913c810a0
ThomasAugusto <thomas.ramos@estudante.senairs.edu.br>
2023-11-14 22:14:10 -0300

    version 1.4.3

--- app/src/main/java/com/example/trabalho_final_pdm/Account_form.java
+++ app/src/main/java/com/example/trabalho_final_pdm/Account_form.java
@@ -3,7 +3,9 @@ package com.example.trabalho_final_pdm;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +15,16 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.santalu.maskara.Mask;
+import com.santalu.maskara.MaskChangedListener;
+import com.santalu.maskara.MaskStyle;
+
 public class Account_form extends AppCompatActivity {
 
 Spinner spinner;
 EditText edit_text;
 AppCompatButton btn_submib;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +32,51 @@ AppCompatButton btn_submib;
         this.getSupportActionBar().hide();
         StartComponents();
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectOP = (String) parent.getItemAtPosition(position);
+                afterSpinnerChange(selectOP);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+    private void afterSpinnerChange(String selectOP){
+        if (selectOP.equals("CPF")){
+            MaskCPF();
+        }else {
+            MasknumPhone();
+        }
     }
 
+    private void MaskCPF(){
+            Mask mask = new Mask(
+                    "___.___.___-__",
+                    '_',
+                    MaskStyle.NORMAL
+            );
+            MaskChangedListener listener = new MaskChangedListener(mask);
+            edit_text.setText("");
+            edit_text.removeTextChangedListener(listener);
+            edit_text.addTextChangedListener(listener);
+            edit_text.setHint("Numero de CPF");
+    }
+    private void MasknumPhone(){
+            Mask mask = new Mask(
+                    "(__) _____-____",
+                    '_',
+                    MaskStyle.NORMAL
+            );
+            MaskChangedListener listener = new MaskChangedListener(mask);
+            edit_text.setText("");
+            edit_text.removeTextChangedListener(listener);
+            edit_text.addTextChangedListener(listener);
+            edit_text.setHint("Numero com DDD");
+    }
     private void StartComponents(){
         spinner=findViewById(R.id.account_spinner);
         edit_text=findViewById(R.id.editText_account);
