08b1feb51a7719ceaac467e674a494b53b31c46e
Israel <israel.andreotti01@gmail.com>
2023-11-28 20:55:24 -0300

    v4.5

--- app/src/main/java/com/example/trabalho_final_pdm/PixAdapter.java
+++ app/src/main/java/com/example/trabalho_final_pdm/PixAdapter.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trabalho_final_pdm.model.PixItem;
